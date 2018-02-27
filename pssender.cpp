9c4f220c2776a8277d3b9cf18b3b34effbcae873
Daniil Meytis <dsmejtis@npotelecom.ru>
2018-02-27 09:17:48 +0400

    manual /proc parsing

--- pssender.cpp
+++ pssender.cpp
@@ -7,11 +7,13 @@
 //============================================================================
 
 #include <iostream>
+#include <fstream>
 #include <cerrno>
 #include <csignal>
 #include <cstring>
 #include <cstdio>
 #include <unistd.h>
+#include <dirent.h>
 #include <curl/curl.h>
 
 #include "json.hpp"
@@ -25,9 +27,61 @@ template<class K, class V, class dummy_compare, class A>
 using my_workaround_fifo_map = nlohmann::fifo_map<K, V, nlohmann::fifo_map_compare<K>, A>;
 using my_json = nlohmann::basic_json<my_workaround_fifo_map>;
 
-size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp)
-{
+size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp) {
    return size * nmemb;
+}
+
+my_json procps_parse() {
+	DIR *proc = opendir("/proc");
+	my_json j;
+	for (;;) {
+		unsigned pid, uid;
+		string fil, cmd, buffer;
+		fstream fs;
+		bool kernel;
+
+		struct dirent *entry = readdir(proc);
+		if (entry == NULL)
+			break;
+
+		pid = strtoul(entry->d_name, NULL, 0);
+		if (!pid)
+			continue;
+
+		fil = "/proc/" + to_string(pid) + "/cmdline";
+		fs.open(fil.c_str(), fstream::in);
+		if (!fs.is_open())
+			perror("error while opening file");
+		fs >> cmd;
+		kernel = cmd.empty();
+		fs.close();
+
+		fil = "/proc/" + to_string(pid) + "/status";
+		fs.open(fil.c_str(), fstream::in);
+		if (!fs.is_open())
+			perror("error while opening file");
+		while (getline(fs, buffer)) {
+			if (kernel)
+				if (buffer.find("Name:") != string::npos)
+					cmd = buffer.substr(6);
+			if (buffer.find("Uid:") != string::npos) {
+				uid = stoi(buffer.substr(4));
+				break;
+			}
+		}
+		fs.close();
+
+		/* find and screen special JSON symbols in cmd */
+		size_t found = cmd.find_first_of("\"\\");
+		while (found != string::npos) {
+			cmd.insert(found, 1, '\\');
+			found = cmd.find_first_of("\"\\", found + 1);
+		}
+
+		j += {{"uid", uid}, {"pid", pid}, {"cmd", cmd.data()}};
+	}
+	closedir(proc);
+	return j;
 }
 
 int main(int argc, char** argv) {
@@ -50,24 +104,9 @@ int main(int argc, char** argv) {
 	curl_easy_setopt(curl, CURLOPT_PROXY, "127.0.0.1:9050");
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data); /* dummy func to ignore POST response */
 
+
 	for (;;) {
-		FILE *ps = popen("ps l", "r");
-		if (ps == nullptr) {
-			perror("Unable to get current processes list");
-			return -1;
-		}
-		char buf[MAXLINE];
-		int d, uid, pid;
-		char s[10], ccmd[64];
-		my_json j;
-		fgets(buf, MAXLINE, ps);
-		while (fgets(buf, MAXLINE, ps) != nullptr) {
-			sscanf(buf, "%d %d %d %d %d %d %d %d %s %s %s %s %s",
-					&d, &uid, &pid, &d, &d, &d, &d, &d, s, s, s, s, ccmd);
-			string cmd(ccmd);
-			j += {{"uid", uid}, {"pid", pid}, {"cmd", cmd}};
-		}
-		pclose(ps);
+		my_json j = procps_parse();
 		string js = j.dump();
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, js.data());
 
